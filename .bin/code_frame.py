5d915b9891f0b940bcaa6a43b771c373522bea5d
Grzegorz Gwozdz <gregoryprogrammer@gmail.com>
2019-04-26 19:54:15 +0200

    code_frame.py

--- .bin/code_frame.py
+++ .bin/code_frame.py
@@ -9,12 +9,47 @@
 
 import sys
 
-text = ' '.join(sys.argv[1:])
+def framed(text, width):
+    top = '.' + '-' * (width - 2) + '.'
+    mid = '|' + ' ' * (width - 2) + '|'
+    btm = "'" + '-' * (width - 2) + "'"
 
-fil  = f" * |{text.center(74)}|"
+    rows = text.split(chr(0x0d))  # list of rows
+    max_length = max(len(row) for row in rows)
+    rows = [row.ljust(max_length) for row in rows]
 
-top  = "/* ." + ("-" * 74) + "."
-mid  = " * |" + (" " * 74) + "|"
-btm  = " * '" + ("-" * 74) + "'\n */"
+    result = []
+    result.append(top)
+    result.append(mid)
+    for row in rows:
+        result.append( '|' + row.center(width - 2) + '|')
+    result.append(mid)
+    result.append(btm)
 
-print('\n'.join([top, mid, fil, mid, btm]))
+    return result
+
+def cpp_comment(rows):
+    result = []
+    result.append('')
+    result.append('/* ' + rows[0])
+    for row in rows[1:]:
+        result.append(' * ' + row)
+    result.append(' */')
+    return result
+
+def python_comment(rows):
+    result = []
+    result.append('')
+    for row in rows:
+        result.append('# ' + row)
+    return result
+
+CPP_WIDTH = 76
+PYTHON_WIDTH = 77
+
+text = ' '.join(sys.argv[1:]).strip()
+
+print('\n'.join(cpp_comment(framed(text, CPP_WIDTH))))
+
+# TODO argparse python
+#  print('\n'.join(python_comment(framed(text, PYTHON_WIDTH))))
