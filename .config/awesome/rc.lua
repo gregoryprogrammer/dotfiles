9363595940bdd3d8af37ed94e9b556dee7047ab0
Grzegorz Gwozdz <gregoryprogrammer@gmail.com>
2019-11-21 22:03:51 +0100

    remove compton

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -571,6 +571,3 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
-
--- Startup apps
-awful.util.spawn("compton --config " .. os.getenv("HOME") .. "/.config/awesome/compton.conf")
